6a61f6dd975b8c811c4d24c16eb88d8ef63f13e0
Adrien_Medinger <adrien.medinger@gmail.com>
2026-03-23 20:53:46 +0100

    tutorial1_3

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -23,7 +23,7 @@ def vote(request, question_id):
 
 
 
-from django.http import HttpResponse
+
 
 from .models import Question
 
@@ -35,3 +35,39 @@ def index(request):
 
 
 # Leave the rest of the views (detail, results, vote) unchanged
+
+
+
+
+from django.template import loader
+from django.shortcuts import render 
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {"latest_question_list": latest_question_list}
+    return HttpResponse(template.render(context, request))
+
+
+from django.http import Http404
+
+
+# ...
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
+
+
+
+
+from django.shortcuts import get_object_or_404, render
+
+
+# ...
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
