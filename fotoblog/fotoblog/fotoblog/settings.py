dc7441620b037d6d980c63e8249e6eeaad04e165
Adrien_Medinger <adrien.medinger@gmail.com>
2026-05-08 16:00:50 +0200

    tutoriel2

--- fotoblog/fotoblog/fotoblog/settings.py
+++ fotoblog/fotoblog/fotoblog/settings.py
@@ -137,3 +137,23 @@ INSTALLED_APPS = [
     'blog',
 ]
 
+# fotoblog/settings.py
+...
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            BASE_DIR.joinpath('templates'),· # <--- ajoutez cette ligne
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+...
