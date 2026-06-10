1265a33f37d3cf04320820d25b683b8004ee1748
Adrien_Medinger <adrien.medinger@gmail.com>
2026-06-10 14:00:08 +0200

    commit6_projet

--- BloomWay/bloomway/projet/views.py
+++ BloomWay/bloomway/projet/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from . import forms 
-from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.shortcuts import redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.conf import settings 
-
+from .models import produit, variante_produit, User
 
 
 def login_page(request):
@@ -44,3 +44,8 @@ def signup_page(request):
             user =form.save()
             return redirect('login')
     return render(request, 'projet/signup.html', context={'form': form})
+
+def affichage_produit(request, produit_id):
+    categorie = produit.objects.get(id=produit_id)
+    produits = variante_produit.objects.filter(categorie=categorie)
+    return render(request, 'projet/affichage_produit.html', {'produits': produits, 'categorie': categorie})
